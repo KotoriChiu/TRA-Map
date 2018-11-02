1c08ac1fd2ee266f8a31bae9ead1dbf54e0bf8df
KotoriChiu <zxcv88928@gmail.com>
2018-11-02 10:29:59 +0800

    修改code寫法owo

---
+++ RailStation.java
@@ -0,0 +1,72 @@
+
+
+public class RailStation {
+
+   
+    public String StationID;
+    
+    public String StationNameZh;
+    
+    public String StationNameEn;
+    
+    public long StationLon;
+    
+    public long StationLat;
+    
+    public String StationAddress;
+    
+    public String StationPhone;
+    
+    public String StationClass;
+    
+    
+	public String getStationID() {
+		return StationID;
+	}
+	public void setStationID(String stationID) {
+		StationID = stationID;
+	}
+	public String getStationNameZh() {
+		return StationNameZh;
+	}
+	public void setStationNameZh(String stationNameZh) {
+		StationNameZh = stationNameZh;
+	}
+	public String getStationNameEn() {
+		return StationNameEn;
+	}
+	public void setStationNameEn(String stationNameEn) {
+		StationNameEn = stationNameEn;
+	}
+	public long getStationLon() {
+		return StationLon;
+	}
+	public void setStationLon(long stationLon) {
+		StationLon = stationLon;
+	}
+	public long getStationLat() {
+		return StationLat;
+	}
+	public void setStationLat(long stationLat) {
+		StationLat = stationLat;
+	}
+	public String getStationAddress() {
+		return StationAddress;
+	}
+	public void setStationAddress(String stationAddress) {
+		StationAddress = stationAddress;
+	}
+	public String getStationPhone() {
+		return StationPhone;
+	}
+	public void setStationPhone(String stationPhone) {
+		StationPhone = stationPhone;
+	}
+	public String getStationClass() {
+		return StationClass;
+	}
+	public void setStationClass(String stationClass) {
+		StationClass = stationClass;
+	}
+
+}
