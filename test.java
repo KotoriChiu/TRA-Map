04da1437b8598fe51e93e51afb6fa27914113306
KotoriChiu <zxcv88928@gmail.com>
2019-03-06 09:37:19 +0800

    新增測試檔test.java 測試系統時間運用&研究用

---
+++ test.java
@@ -0,0 +1,8 @@
+
+import java.util.Calendar;
+public class test{
+    public static void main(String args []){
+        Calendar date = Calendar.getInstance() ;
+        System.out.print(date.get(Calendar.MONTH));
+    }
+}
